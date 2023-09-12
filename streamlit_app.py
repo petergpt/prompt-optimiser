7cb15b8207d8eceb2b2c074e153c1a270648b9e3
petergpt <130558610+petergpt@users.noreply.github.com>
2023-09-12 06:05:57 +0000

    state fix issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,38 +5,43 @@ from conversation_simulation import simulate_conversation, generate_prompts, gen
 if 'selected_prompt' not in st.session_state:
     st.session_state.selected_prompt = None
 
-if 'task' not in st.session_state:
-    st.session_state.task = "Create ideas for a B2B company to apply Generative AI"
+if 'generated_prompts' not in st.session_state:
+    st.session_state.generated_prompts = []
 
-if 'num_prompts' not in st.session_state:
-    st.session_state.num_prompts = 4
+if 'generated_responses' not in st.session_state:
+    st.session_state.generated_responses = []
+
+if 'evaluation' not in st.session_state:
+    st.session_state.evaluation = ""
 
 st.title("GPT-4 Prompt Optimizer")
 
 # Widgets
-st.session_state.task = st.text_area("Enter your task:", st.session_state.task)
-st.session_state.num_prompts = st.slider("Number of Prompts:", min_value=2, max_value=10, value=st.session_state.num_prompts)
+task = st.text_area("Enter your task:", "Create ideas for applying Generative AI in a B2B company")
+num_prompts = st.slider("Number of Prompts:", min_value=2, max_value=10, value=4)
 
 if st.button("Generate Optimized Prompt"):
-    with st.spinner('Generating Prompts...'):
-        generated_prompts = generate_prompts(st.session_state.task, st.session_state.num_prompts, st.session_state.selected_prompt)
-    st.write("Generated Prompts:")
-    st.write(generated_prompts)
-    
-    with st.spinner('Generating Responses...'):
-        generated_responses = generate_responses(generated_prompts, st.session_state.task)
+    # Stage 1: Generate Prompts
+    with st.spinner('Stage 1: Generating Prompts...'):
+        st.session_state.generated_prompts = generate_prompts(task, num_prompts, st.session_state.selected_prompt)
+        st.write("Generated Prompts:")
+        st.write(st.session_state.generated_prompts)
+
+    # Stage 2: Generate Responses
+    with st.spinner('Stage 2: Generating Responses...'):
+        st.session_state.generated_responses = generate_responses(st.session_state.generated_prompts, task)
     st.write("Generated Responses:")
-    st.write(generated_responses)
-    
-    with st.spinner('Evaluating Responses...'):
-        evaluation = evaluate_responses(generated_responses, st.session_state.task)
+    st.write(st.session_state.generated_responses)
+        
+    # Stage 3: Evaluate Responses
+    with st.spinner('Stage 3: Evaluating Responses...'):
+        st.session_state.evaluation = evaluate_responses(st.session_state.generated_responses, task)
     st.write("Evaluation:")
-    st.markdown(evaluation)
+    st.markdown(st.session_state.evaluation)
 
-    # Radio buttons for prompt selection
+# Show radio buttons and button only if prompts have been generated
+if st.session_state.generated_prompts:
     st.write("Choose the prompt for the next iteration:")
-    st.session_state.selected_prompt = st.radio("", generated_prompts, index=generated_prompts.index(st.session_state.selected_prompt) if st.session_state.selected_prompt in generated_prompts else 0)
-
-    # Button for running another iteration
+    st.session_state.selected_prompt = st.radio("", st.session_state.generated_prompts)
     if st.button("Run Another Iteration"):
         st.experimental_rerun()

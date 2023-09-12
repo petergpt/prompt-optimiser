37049b5c3aa2e0f4927d740ec0fd937e6c4122e0
petergpt <130558610+petergpt@users.noreply.github.com>
2023-09-12 14:32:24 +0000

    updated the wrong file

--- conversation_simulation.py
+++ conversation_simulation.py
@@ -1,49 +1,58 @@
-import streamlit as st
-from conversation_simulation import generate_prompts, generate_responses, evaluate_responses
+from concurrent.futures import ThreadPoolExecutor, as_completed
+import openai
 
-# Session state initialization if not already done
-if "selected_prompt" not in st.session_state:
-    st.session_state.selected_prompt = None
-if "task" not in st.session_state:
-    st.session_state.task = "Create ideas for applying Generative AI in a B2B company"
-if "num_prompts" not in st.session_state:
-    st.session_state.num_prompts = 4
-if 'generated_prompts' not in st.session_state:
-    st.session_state.generated_prompts = []
-if 'generated_responses' not in st.session_state:
-    st.session_state.generated_responses = []
-if 'evaluation' not in st.session_state:
-    st.session_state.evaluation = ""
+openai.log='debug'
 
-# Streamlit UI
-st.title("GPT-4 Prompt Optimizer")
+def generate_prompts(task, num_prompts=4, existing_prompt=None):
+    prompts = []
+    with ThreadPoolExecutor(max_workers=num_prompts) as executor:
+        future_to_prompt = {executor.submit(openai.ChatCompletion.create,
+                                            model="gpt-4",
+                                            messages=[{"role": "system", 
+                                                       "content": f"Generate one system prompt for the task, take a deep breath and be creative: {task}"}]
+                                           ): i for i in range(num_prompts)}
+        for future in as_completed(future_to_prompt):
+            try:
+                prompt = future.result()['choices'][0]['message']['content'].strip()
+                prompts.append(prompt)
+            except Exception as exc:
+                print('%r generated an exception: %s' % (future_to_prompt[future], exc))
 
-# Widgets
-st.session_state.task = st.text_area("Enter your task:", st.session_state.task)
-st.session_state.num_prompts = st.slider("Number of Prompts:", min_value=2, max_value=10, value=st.session_state.num_prompts)
+    if existing_prompt:
+        prompts.append(existing_prompt)
+    return prompts
 
-placeholder_for_prompt = st.empty()
-placeholder_for_response = st.empty()
-placeholder_for_evaluation = st.empty()
+def generate_responses(prompts, task):
+    responses = []
+    if not prompts:  # check if prompts is empty
+        return responses
+    with ThreadPoolExecutor(max_workers=len(prompts)) as executor:
+        future_to_response = {executor.submit(openai.ChatCompletion.create,
+                                              model="gpt-4",
+                                              messages=[{"role": "system", "content": f"{prompt}: {task}"}],
+                                              max_tokens=500,
+                                              top_p=1,
+                                              frequency_penalty=0,
+                                              presence_penalty=0
+                                             ): prompt for prompt in prompts}
+        for future in as_completed(future_to_response):
+            try:
+                response = future.result()['choices'][0]['message']['content'].strip()
+                responses.append(response)
+            except Exception as exc:
+                print('%r generated an exception: %s' % (future_to_response[future], exc))
+            
+    return responses
 
-# Start button
-if st.button("Start the Process"):
+def evaluate_responses(responses, task):
+    if not responses:  # check if responses is empty
+        return ""
+    evaluation_prompt = f"Evaluate the quality of the following responses for the task '{task}', giving a score between 1 and 5 (5 being the highest). Create a markdown table showing the feedback and the results.:\n"
+    for i, response in enumerate(responses):
+        evaluation_prompt += f"Response {i+1}: {response}\n"
     
-    with st.spinner('Generating Prompts...'):
-        st.session_state.generated_prompts = generate_prompts(st.session_state.task, st.session_state.num_prompts, st.session_state.selected_prompt)
-        placeholder_for_prompt.write("Generated Prompts:")
-        placeholder_for_prompt.write(st.session_state.generated_prompts)
-
-    # Radio buttons to select prompt for next round
-    if st.session_state.generated_prompts:
-        st.session_state.selected_prompt = st.radio("Choose a prompt for the next round:", st.session_state.generated_prompts)
-
-    with st.spinner('Generating Responses...'):
-        st.session_state.generated_responses = generate_responses(st.session_state.generated_prompts, st.session_state.task)
-        placeholder_for_response.write("Generated Responses:")
-        placeholder_for_response.write(st.session_state.generated_responses)
-
-    with st.spinner('Evaluating Responses...'):
-        st.session_state.evaluation = evaluate_responses(st.session_state.generated_responses, st.session_state.task)
-        placeholder_for_evaluation.markdown("Evaluation:")
-        placeholder_for_evaluation.markdown(st.session_state.evaluation)
+    evaluation = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[{"role": "system", "content": evaluation_prompt}]
+    )
+    return evaluation['choices'][0]['message']['content'].strip()
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,17 @@
-# streamlit_app.py
 import streamlit as st
 from conversation_simulation import generate_prompts, generate_responses, evaluate_responses
 
-# Initialize session state
-if 'selected_prompt' not in st.session_state:
+# Session state initialization if not already done
+if "selected_prompt" not in st.session_state:
     st.session_state.selected_prompt = None
-
-if 'task' not in st.session_state:
+if "task" not in st.session_state:
     st.session_state.task = "Create ideas for applying Generative AI in a B2B company"
-
-if 'num_prompts' not in st.session_state:
+if "num_prompts" not in st.session_state:
     st.session_state.num_prompts = 4
-
 if 'generated_prompts' not in st.session_state:
     st.session_state.generated_prompts = []
-
 if 'generated_responses' not in st.session_state:
     st.session_state.generated_responses = []
-
 if 'evaluation' not in st.session_state:
     st.session_state.evaluation = ""
 
@@ -32,12 +26,17 @@ placeholder_for_prompt = st.empty()
 placeholder_for_response = st.empty()
 placeholder_for_evaluation = st.empty()
 
+# Start button
 if st.button("Start the Process"):
-
+    
     with st.spinner('Generating Prompts...'):
-        st.session_state.generated_prompts = generate_prompts(st.session_state.task, st.session_state.num_prompts)
+        st.session_state.generated_prompts = generate_prompts(st.session_state.task, st.session_state.num_prompts, st.session_state.selected_prompt)
         placeholder_for_prompt.write("Generated Prompts:")
         placeholder_for_prompt.write(st.session_state.generated_prompts)
+
+    # Radio buttons to select prompt for next round
+    if st.session_state.generated_prompts:
+        st.session_state.selected_prompt = st.radio("Choose a prompt for the next round:", st.session_state.generated_prompts)
 
     with st.spinner('Generating Responses...'):
         st.session_state.generated_responses = generate_responses(st.session_state.generated_prompts, st.session_state.task)
